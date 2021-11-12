b76726d0da8f40ef4d187e1d5cc4eb71daeac8e9
Misha Krasnonos <muzishutka7@gmail.com>
2021-11-12 23:56:41 +0200

    ok

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+require('dotenv').config()
 
 const contactsRouter = require('./routes/api/contacts')
 
+const e = process.env
+const PORT = e.PORT || 8083
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
@@ -22,4 +25,10 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message })
 })
 
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error('Error at a server launch', err)
+  }
+  console.log(`Server work at port ${PORT}!`)
+})
 module.exports = app
