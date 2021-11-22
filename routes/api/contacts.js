06900fc396b818c17a329b3345d6642fdec214a1
Misha Krasnonos <muzishutka7@gmail.com>
2021-11-22 16:11:19 +0200

    up

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,13 +1,21 @@
 const { NotFound, BadRequest } = require('http-errors')
 const express = require('express')
 const Joi = require('joi')
-const { listContacts, getContactById, addContact } = require('../../model/index')
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  updateContact,
+  removeContact,
+} = require('../../model/index')
 const router = express.Router()
 
 const schema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
-  phone: Joi.number().min(10).max(10).required()
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+    .required(),
+  phone: Joi.string().min(12).max(14).required(),
 })
 
 router.get('/', async (_, res, next) => {
@@ -45,12 +53,34 @@ router.post('/', async (req, res, next) => {
   }
 })
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template mesijsage' })
+router.put('/:contactId', async (req, res, next) => {
+  try {
+    const { error } = schema.validate(req.body)
+    if (error) {
+      throw new BadRequest('missing fields')
+    }
+    const { contactId } = req.params
+    const [result] = await updateContact(contactId, req.body)
+    if (!result) {
+      throw new NotFound(`Not found id = ${contactId}`)
+    }
+    res.status(200).json({ status: 'succes', code: 200, data: { result } })
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.delete('/:contactId', async (req, res, next) => {
+  try {
+    const { contactId } = req.params
+    const [result] = await removeContact(contactId)
+    if (!result) {
+      throw new NotFound(`Not found id = ${contactId}`)
+    }
+    res.json({ status: 'succes', code: 200, message: 'contact deleted' })
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
